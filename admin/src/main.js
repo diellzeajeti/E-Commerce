99e43a535c633488961639108adb7b0d11099a11
Diellza Ajeti <diellzaajeti13@gmail.com>
2023-04-23 15:49:43 +0200

    Small changes

--- admin/src/main.js
+++ admin/src/main.js
@@ -2,9 +2,10 @@ import { createApp } from 'vue'
 import store from './store'
 import router from './router'
 import './index.css';
+
 import App from './App.vue'
 
-import './assets/main.css'
+
 
 createApp(App)
     .use(store)
