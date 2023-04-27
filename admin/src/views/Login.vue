9dc2ddbbc1d5e3f51ddd00ea9a683655c9237c72
Blerina Rushiti <blerinarushiti94@gmail.com>
2023-04-28 02:05:56 +0800

    Added Splash Screen Loader

--- admin/src/views/Login.vue
+++ admin/src/views/Login.vue
@@ -41,11 +41,27 @@
                           }"
                           >
 
-                          <svg v-if="loading" class="animate-spin -ml-1 mr-3 h-5 w-5 text-white" xmlns="http://www.w3.org/2000/svg" fill="none"
-                             viewBox="0 0 24 24">
-                             <circle class="opacity-25" cx="12" cy="12" r="10" storke="currentColor" storke-width="4"></circle>
-                             <path class="opacity-75" fil="currentColor"
-                               d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 0 014 12H0c0 3.042 1.135  5.824 3 7.93813-2.647z"></path>
+                          <svg 
+                          class="animate-spin -ml-1 mr-3 h-5 w-5 text-white" 
+                          xmlns="http://www.w3.org/2000/svg" 
+                          fill="none"
+                          viewBox="0 0 24 24"
+                          >
+                             
+                             <circle
+                              class="opacity-25"
+                               cx="12" 
+                               cy="12" 
+                               r="10" 
+                               storke="currentColor" 
+                               storke-width="4"
+                               ></circle>
+                             
+                             <path 
+                             class="opacity-75"
+                              fil="currentColor"
+                              d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 0 014 12H0c0 3.042 1.135  5.824 3 7.93813-2.647z"
+                              ></path>
                          </svg> 
                        
                       <span class="absolute left-0 inset-y-0 flex items-center pl-3">
