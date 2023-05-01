9102da3f95b7f0e75e297f8c17b0715215c1a574
Diellza Ajeti <diellzaajeti13@gmail.com>
2023-05-01 21:59:07 +0200

    Product Delete

--- admin/src/views/Products/ProductModal.vue
+++ admin/src/views/Products/ProductModal.vue
@@ -56,8 +56,8 @@
                           </div>
                         <footer class="bg-gray-50 px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse">
                           <button type="submit"
-                         class="mt-3 w-full inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-base font-medium  focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm
-                          bg-indigo-600 hover:bg-indigo-700  focus:ring-indigo-500 text-white">
+                         class="mt-3 w-full inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2  text-base font-medium  focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm
+                          bg-indigo-600 hover:bg-indigo-700  text-white">
                          Submit
                           </button>
                           <button type="button"
