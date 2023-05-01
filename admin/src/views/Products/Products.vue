a36620e33733a80d2320385cae4a827712e513ba
Diellza Ajeti <diellzaajeti13@gmail.com>
2023-05-01 23:09:47 +0200

    Product Update

--- admin/src/views/Products/Products.vue
+++ admin/src/views/Products/Products.vue
@@ -8,28 +8,42 @@
         Add new Product
       </button>
     </div>
-    <ProductModal v-model="showModal" :product="productModel"/>
-    <ProductsTable/>
+    <ProductModal v-model="showModal" :product="productModel" @close="onModalClose"/>
+    <ProductsTable @clickEdit="editProduct"/>
 </template>
    
    <script setup>
    import ProductsTable from "./ProductsTable.vue"
    import ProductModal from "./ProductModal.vue"
    import {ref} from "vue";
+   import store from "../../store";
 
-   const showModal = ref(false)
-   const productModel = ref({
+   const DEFAULT_EMPTY_OBJECT = {
     id:'',
     title:'',
     image:'',
     description:'',
     price:'',
-   })
+   }
+   const showModal = ref(false)
+   const productModel = ref({...DEFAULT_EMPTY_OBJECT})
 
 
    function showProductModal(){
        showModal.value = true
    }
+
+   function editProduct(product) {
+    store.dispatch('getProduct', product.id)
+      .then(({data}) => {
+        productModel.value = data
+        showProductModal()
+      })
+   }
+
+  function onModalClose(){
+    productModel.value = {...DEFAULT_EMPTY_OBJECT}
+  }
    </script>
    
    <style scoped>
