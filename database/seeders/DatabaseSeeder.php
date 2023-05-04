c9521f0f3c2601bf29f65086aaa9ae34eaa1914f
Blerta Mucolli <blertamucolli159@gmail.com>
2023-05-04 17:04:33 +0200

    Implemented email verification

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,8 +13,7 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         $this->call([
-            AdminUserSeeder::class,
-            ProductSeeder::class
+            AdminUserSeeder::class
         ]);
         // \App\Models\User::factory(10)->create();
 
