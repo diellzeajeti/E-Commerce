43c2278481a0b1ec68ca84d3ebe9ffaec1c45084
Albulena Halimi <albulenahalimi4@gmail.com>
2023-05-20 11:34:07 +0200

    Cart Controller

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\CartController;
 use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
 
@@ -14,12 +15,21 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', [\App\Http\Controllers\ProductController::class, 'index'])->name('home');
-Route::get('/product/{product:slug}', [ProductController::class, 'view']) ->name('product.view');
+Route::middleware(['guestOrVerified'])->group(function() {
+    Route::get('/', [ProductController::class, 'index'])->name('home');
+    Route::get('/product/{product::slug}', [ProductController::class, 'view'])->name('product.view');
+    
+    Route::prefix('/cart')->name('cart.')->group(function(){
+        Route::get('/', [CartController::class, 'index'])->name('index');
+        Route::get('/add/{product:slug}', [CartController::class, 'add'])->name('add');
+        Route::get('/remove/{product:slug}', [CartController::class, 'remove'])->name('remove');
+        Route::get('/update-quantity/{product:slug}', [CartController::class, 'updateQuantity'])->name('update-quantity');
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+    });
+});
+
+
+
 
 
 
