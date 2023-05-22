9369bc9e7947767c7c9a21cffa7031833de59fc9
Diellza Ajeti <diellzaajeti13@gmail.com>
2023-05-22 13:48:14 +0200

    .

--- app/Http/Requests/PasswordUpdateRequest.php
+++ app/Http/Requests/PasswordUpdateRequest.php
@@ -3,8 +3,9 @@
 namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rules\Password;
 
-class PasswordUpdatRequest extends FormRequest
+class PasswordUpdateRequest extends FormRequest
 {
     /**
      * Determine if the user is authorized to make this request.
