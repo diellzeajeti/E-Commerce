ab637b361c97448579925e0908dbc246fb884f37
Blerta Mucolli <blertamucolli159@gmail.com>
2023-05-25 02:33:04 +0200

    Order List Page

--- app/Http/Controllers/CheckoutController.php
+++ app/Http/Controllers/CheckoutController.php
@@ -123,6 +123,14 @@ class CheckoutController extends Controller
     public function failure(Request $request)
     {
         
-        dd($request->all());
+        return view('checkout.failure', ['message' => ""]);
+    }
+
+    public function checkoutOrder(Order $order, Request $request)
+    {
+             /** @var \App\Models\User $user */
+		     $user = $request->user();
+             $stripe = new \Stripe\StripeClient(getenv('STRIPE_SECRET_KEY'));
+
     }
 }
