9d792393e6865431157ddd45a0068618ab67a2df
Albulena Halimi <albulenahalimi4@gmail.com>
2023-05-26 11:20:28 +0200

    Webhooks

--- app/Http/Controllers/CheckoutController.php
+++ app/Http/Controllers/CheckoutController.php
@@ -2,7 +2,7 @@
 
 namespace App\Http\Controllers;
 
-
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use App\Http\Controllers\Controller;
 use App\Http\Helpers\Cart;
 use Exception;
@@ -92,7 +92,7 @@ class CheckoutController extends Controller
 
           Payment::create($paymentData);
 
-        
+          CartItem::where(['user_id' => $user->id])->delete();
 
           return redirect($session->url);
     }
@@ -111,27 +111,26 @@ class CheckoutController extends Controller
             if(!$session){
                 return view('checkout.failure', ['message' => 'Invalid Session ID']);
             }
-           $payment = Payment::query()->where(['session_id' => $session->id, 'status' => PaymentStatus::Pending])->first();
-          
-           if(!$payment){
-            return view('checkout.failure', ['message' => 'Payment Does not exist']);
-        }
-            $payment->status = PaymentStatus::Paid;
-            $payment->update();
+           
+           $payment = Payment::query()
+           ->where(['session_id' => $session_id])
+           whereIn( 'status', [PaymentStatus::Pending, PaymentStatus::Paid])
+           ->first();
+           if (!$payment) {
+            throw new NotFoundedHttpException();
+           }
 
-            $order = $payment->order;
+           if($payment->status === PaymentStatus::Pending) {
+            $this->updateOrderAndSession($payment);
             
+           }
 
-
-            $order->status = OrderStatus::Paid;
-            $order->update();
-
-            CartItem::where(['user_id' => $user->id])->delete();
-
+           
            $customer = $stripe->customers->retrieve($session->customer);
 
             return view('checkout.success', compact('customer'));
         } catch(Exception $e){
+            throw $e;
             return view('checkout.failure', ['message' => $e->getMessage()]);
         }
     }
@@ -144,8 +143,7 @@ class CheckoutController extends Controller
 
     public function checkoutOrder(Order $order, Request $request)
     {
-             /** @var \App\Models\User $user */
-		     $user = $request->user();
+            
              $stripe = new \Stripe\StripeClient(getenv('STRIPE_SECRET_KEY'));
 
              $lineItems = [];
@@ -172,8 +170,75 @@ class CheckoutController extends Controller
                 'cancel_url' => route('checkout.failure', [], true),
                 'customer_creation'=>'always',
               ]);
-            $order->payment->session_id = $session->id;
+
+        $order->payment->session_id = $session->id;
         $order->payment->save();
-              return redirect($session->url);
+
+        return redirect($session->url);
     }
+
+    public function webhook()
+    {
+        
+        $stripe = new \Stripe\StripeClient(getenv('STRIPE_SECRET_KEY'));
+
+        $endpoint_secret = 'whsec_6de6916c6587d69e8e84fd6bfe9ca64e25d12bf5c750b149f090228b262c33c6'; 
+
+       $payload = @file_get_contents('php://input');
+       $sig_header = $_SERVER['HTTP_STRIPE_SIGNATURE'];
+       $event = null;
+
+  try {
+       $event = \Stripe\Webhook::constructEvent(
+          $payload, $sig_header, $endpoint_secret
+  );
+} catch(\UnexpectedValueException $e) {
+  // Invalid payload
+  return response('', 401);
+  exit();
+} catch(\Stripe\Exception\SignatureVerificationException $e) {
+  // Invalid signature
+  return response('', 402);
+  exit();
+}
+
+// Handle the event
+switch ($event->type) {
+  case 'checkout.session.completed':
+    $paymentIntent = $event->data->object;
+    $sessionId = $paymentIntent['id'];
+
+    $payment = Payment::query()
+    ->where(['session_id' => $sessionId, 'status' => PaymentStatus::Pending])
+    ->first();
+           if ($payment) {
+            $this->updateOrderAndSession($payment);
+           }
+
+   
+
+  // ... handle other event types
+  default:
+    echo 'Received unknown event type ' . $event->type;
+}
+
+     return response('', 200);
+
+  }
+
+  private function updateOrderAndSession(Payment $payment)
+  {
+    
+            $payment->status = PaymentStatus::Paid;
+            $payment->update();
+
+            $order = $payment->order;
+            
+
+
+            $order->status = OrderStatus::Paid;
+            $order->update();
+  }
+
+
 }
