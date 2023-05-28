f4f5a539ac3ddde67d90126f45764d2cbd84fabc
Diellza Ajeti <diellzaajeti13@gmail.com>
2023-05-28 02:45:35 +0200

    Implementation of Order status update

---
+++ admin/src/views/Orders/OrderStatus.vue
@@ -0,0 +1,17 @@
+<template>
+    <span class="text-white py-1 px-2 rounded" :class="{
+        'bg-emerald-500': ['paid', 'completed'].includes(order.status),
+        'bg-orange-400': order.status === 'shipped',
+        'bg-gray-400': order.status === 'unpaid',
+        'bg-red-500': order.status === 'cancelled',
+     }">{{ order.status }}</span>
+</template>
+
+<script setup>
+    const { order } = defineProps({
+        order: Object
+    })
+</script>
+
+<style scoped>
+</style>
--- app/Enums/OrderStatus.php
+++ app/Enums/OrderStatus.php
@@ -11,5 +11,14 @@ enum OrderStatus: string
 {
     case Unpaid = 'unpaid';
     case Paid = 'paid';
+    case Cancelled = 'cancelled';
+    case Shipped = 'shipped';
     case Completed = 'complete';
+
+
+    public static function getStatuses(){
+        return[
+            self::Paid, self::Unpaid, self::Cancelled, self::Shipped, self::Completed
+        ];
+    }
 }
