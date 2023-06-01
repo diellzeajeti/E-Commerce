edb0c3551243e91f5c4237871dafd93f506378f9
Diellza Ajeti <diellzaajeti13@gmail.com>
2023-06-02 00:08:56 +0200

    Changes..

--- app/Http/Controllers/Api/CustomerController.php
+++ app/Http/Controllers/Api/CustomerController.php
@@ -2,16 +2,13 @@
 
 namespace App\Http\Controllers\Api;
 
+use App\Enums\AddressType;
 use App\Http\Controllers\Controller;
-use App\Models\Api\Product;
-use App\Http\Requests\ProductRequest;
-use Illuminate\Http\Request;
-use App\Http\Resources\ProductListResource;
-use App\Http\Resources\ProductResource;
-use Illuminate\Support\Facades\Storage;
-use Illuminate\Support\Facades\URL;
-use Illuminate\Support\Str;
-use Illuminate\Http\UploadedFile;
+use App\Http\Requests\CustomerRequest;
+use App\Models\Customer;
+use App\Models\CustomerAddress;
+use App\Http\Resources\CustomerListResource;
+use App\Http\Resources\CustomerResource;
 
 class ProductController extends Controller
 {
@@ -28,106 +25,71 @@ class ProductController extends Controller
         $sortDirection = request('sort_direction', 'desc');
 
 
-        $query = Product::query()
-        ->where('title', 'like', "%{$search}%")
+        $query = Customer::query()
+       //  ->where('title', 'like', "%{$search}%")
         ->orderBy ($sortField, $sortDirection)
         ->paginate ($perPage);
-        return ProductListResource::collection($query);
+        return CustomerListResource::collection($query);
     }
 
-    /**
-     * Store a newly created resource in storage.
-     * 
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(ProductRequest $request)
-    {
-        $data = $request->validated();
-       $data['created_by'] = $request->user()->id;
-       $data['updated_by'] = $request->user()->id;
-
-       /** @var \Illuminate\Http\UploadedFile $image */
-       $image = $data['image'] ?? null;
-       //Check if img was given and save as local file system
-       if($image) {
-            $relativePath = $this->saveImage($image);
-            $data['image'] = URL::to(Storage::url($relativePath));
-            $data['image_mime'] = $image->getClientMimeType();
-            $data['image_size'] = $image->getSize();
-       }
-       $product = Product::create($data);
-
-       return new ProductResource($product);
-    }
-
+    
     /**
      * Display the specified resource.
      * 
-     * @param  \App\Models\Product  $product
+     * @param  \App\Models\Customer  $customer
      * @return \Illuminate\Http\Response
      */
-    public function show(Product $product)
+    public function show(Customer $customer)
     {
-        return new ProductResource($product);
+        return new CustomerResource($customer);
     }
 
     /**
      * Update the specified resource in storage.
      * 
      * @param \App\Http\Request $request
-     * @param \App\Models\Product $product
+     * @param \App\Models\Customer $customer
      * @return \Illuminate\Http\Response
      */
-    public function update(ProductRequest $request, Product $product)
+    public function update(CustomerRequest $request, Customer $customer)
     {
-        $data = $request->validated();
-        $data['updated_by'] = $request->user()->id;
+        $customerData = $request->validated();
+        $customerData['updated_by'] = $request->user()->id;
+        $shippingData = $customerData['shippingAddress'];
+        $billingData = $customerData['billingAddress'];
+       
+        $customer->update($customerData);
 
-        /** @var \Illuminate\Http\UploadedFile $image */
-        $image = $data['image'] ?? null;
-
-        //Check if img was given and save on local file system
-        if($image) {
-            $relativePath = $this->saveImage($image);
-            $data['image'] = URL::to(Storage::url($relativePath));
-            $data['image_mime'] = $image->getClientMimeType();
-            $data['image_size'] = $image->getSize();
-
-            if($product->image) {
-                Storage::deleteDirectory('/public/' . dirname($product->image));
-            }
+        if ($customer->shippingAddress) {
+            $customer->shippingAddress->update($shippingData);
+        } else {
+            $shippingData['customer_id'] = $customer->user_id;
+            $shippingData['type'] = AddressType::Shipping->value;
+            CustomerAddress::create($shippingData);
+        }
+        if ($customer->billingAddress) {
+            $customer->billingAddress->update($billingData);
+        } else {
+            $billingData['customer_id'] = $customer->user_id;
+            $billingData['type'] = AddressType::Billing->value;
+            CustomerAddress::create($billingData);
         }
 
-        $product->update($data);
-
-        return new ProductResource($product);
+        return new CustomerResource($customer);
     }
 
     /**
      * Remove the specified resource from storage.
      * 
-     * @param  \App\Models\Product  $product
+     * @param  \App\Models\Customer  $customer
      * @return \Illuminate\Http\Response
      */
-    public function destroy(Product $product)
+    public function destroy(Customer $customer)
     {
-        $product->delete();
+        $customer->delete();
 
         return response()->noContent();
     }
-    private function saveImage(UploadedFile $image)
-    {
-        $path = 'images/' . Str::random();
-        if(!Storage::exists($path)){
-            Storage::makeDirectory($path, 0755, true);
-        }
-        if(!Storage::putFileAS('public/' . $path, $image, $image->getClientOriginalName())) {
-            throw new \Exception("Unable to save file \"{$image->getClientOriginalName()}\"");
-        }
-
-        return $path . '/' . $image->getClientOriginalName();
-    }
-
+    
 }
 
