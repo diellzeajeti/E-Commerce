e06d85875af70dc1a3482e8f055ef88e9fa54b8d
Blerta Mucolli <blertamucolli159@gmail.com>
2023-06-01 23:04:37 +0200

    Customer Contoller-API

---
+++ app/Enums/CustomerStatus.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Enums;
+
+
+/**
+ * Class CustomerStatus
+ * @package App\Enums
+ */
+enum CustomerStatus: string
+{
+   
+    case Active = 'active';
+    case Disabled = 'disabled';
+}
