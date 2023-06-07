a12739742818d6b353217565a6ca5993e9042628
Blerina Rushiti <blerinarushiti94@gmail.com>
2023-06-07 18:33:43 +0800

    Report Routes

--- admin/src/components/Sidebar.vue
+++ admin/src/components/Sidebar.vue
@@ -40,7 +40,7 @@
                 Customers
             </span>
         </router-link>
-        <router-link :to="{name: 'app.dashboard'}" class="flex items-center p-2 rounded transition-colors hover:bg-black/30">
+        <router-link :to="{name: 'reports.orders'}" class="flex items-center p-2 rounded transition-colors hover:bg-black/30">
             <span class="mr-2 text-gray-400">
                 <ChartBarIcon class="w-5"/>
             </span>
---
+++ admin/src/views/Reports/Report.vue
@@ -0,0 +1,24 @@
+<template>
+   <div>
+    <router-link to="">
+        Orders Report
+    </router-link>
+
+    <router-link to="">
+        Customers Report
+    </router-link>
+    <router-view/>
+   </div>
+</template>
+
+
+<script>
+export default {
+    name:"OrdersReport.vue"
+}
+
+</script>
+
+<style scoped>
+
+</style>
